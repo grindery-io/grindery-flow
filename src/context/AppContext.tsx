9d0ffe996023b1b92e69f3aca38df5e6c6f3aa22
milukove <milukove@gmail.com>
2022-06-01 01:59:17 +0300

    add sample connector to app context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,28 +1,34 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext } from 'react';
+import gsheetConnector from '../samples/gsheet-connector.json';
 
 type ContextProps = {
     state: any;
     setState?: (a: any) => void;
+    connectors?: any[];
 };
 
 type AppContextProps = {
   children: React.ReactNode;
-}
+};
 
-export const AppContext = createContext<Partial<ContextProps>>({})
+export const AppContext = createContext<Partial<ContextProps>>({});
 
 export const AppContextProvider = ({children}: AppContextProps) => {
   const [state, setState] = useState({})
+  const connectors = [
+    gsheetConnector
+  ]
 
   return (
     <AppContext.Provider value={{ 
       state,
-      setState
+      setState,
+      connectors
     }}>
       {children}
     </AppContext.Provider>
   )
-}
+};
 
 export const useAppContext = () => useContext(AppContext);
 
