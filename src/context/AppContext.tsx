fb197087aa2862e03d39c5b0b1c8756adc518206
milukove <milukove@gmail.com>
2022-06-02 06:54:20 +0300

    update app context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -11,6 +11,20 @@ type ContextProps = {
   setWorkflow: (a: any) => void;
   connectorsWithTriggers: any[];
   connectorsWithActions: any[];
+  triggerIsSet: boolean;
+  actionIsSet: boolean;
+  triggerIsAuthenticated: boolean;
+  triggerIsConfigured: boolean;
+  trigger: any;
+  availableTriggers: any[];
+  availableActions: any[];
+  action: any;
+  triggerConnector: any;
+  actionConnector: any;
+  triggerConnectorIsSet: boolean;
+  actionConnectorIsSet: boolean;
+  triggerConfigSubmitted: boolean;
+  setTriggerConfigSubmitted: (a: any) => void;
 };
 
 type AppContextProps = {
@@ -22,15 +36,13 @@ export const AppContext = createContext<Partial<ContextProps>>({});
 export const AppContextProvider = ({ children }: AppContextProps) => {
   const [state, setState] = useState({});
   const connectors = [gsheetConnector, molochXdaiConnector];
-  const [workflow, setWorkflow] = useState({
+  const [workflow, setWorkflow] = useState<Workflow>({
     title: "New workflow",
     trigger: {
       type: "trigger",
       connector: "",
       operation: "",
       input: {},
-      display: {},
-      authentication: false,
     },
     actions: [
       {
@@ -38,13 +50,12 @@ export const AppContextProvider = ({ children }: AppContextProps) => {
         connector: "",
         operation: "",
         input: {},
-        display: {},
-        authentication: false,
       },
     ],
     creator: "demo:user",
-    signature: "",
   });
+
+  const [triggerConfigSubmitted, setTriggerConfigSubmitted] = useState(false);
 
   const connectorsWithTriggers = connectors?.filter(
     (connector) =>
@@ -54,6 +65,103 @@ export const AppContextProvider = ({ children }: AppContextProps) => {
     (connector) =>
       connector && connector.actions && connector.actions.length > 0
   );
+
+  const triggerIsSet = Boolean(
+    workflow && workflow.trigger && workflow.trigger.operation
+  );
+
+  const actionIsSet = Boolean(
+    workflow &&
+      workflow.actions &&
+      workflow.actions[0] &&
+      workflow.actions[0].operation
+  );
+
+  const triggerConnectorIsSet = Boolean(
+    workflow && workflow.trigger && workflow.trigger.connector
+  );
+
+  const actionConnectorIsSet = Boolean(
+    workflow &&
+      workflow.actions &&
+      workflow.actions[0] &&
+      workflow.actions[0].connector
+  );
+
+  const triggerIsAuthenticated = Boolean(
+    workflow && workflow.trigger && workflow.trigger.authentication
+  );
+
+  const workflowTriggerConnector = workflow?.trigger.connector;
+  const workflowTriggerOperation = workflow?.trigger.operation;
+  const workflowActionConnector = workflow?.actions[0].connector;
+  const workflowActionOperation = workflow?.actions[0].operation;
+
+  const triggerConnector = connectors?.find(
+    (connector) =>
+      connector && connector.name && connector.name === workflowTriggerConnector
+  );
+
+  const trigger = triggerConnector?.triggers.find(
+    (connectorTrigger: { name: any }) =>
+      connectorTrigger && connectorTrigger.name === workflowTriggerOperation
+  );
+
+  const actionConnector = connectors?.find(
+    (connector) =>
+      connector && connector.name && connector.name === workflowActionConnector
+  );
+
+  const action = actionConnector?.actions.find(
+    (connectorAction: { name: any }) =>
+      connectorAction && connectorAction.name === workflowActionOperation
+  );
+
+  const requiredTriggerFields =
+    (trigger &&
+      trigger.operation &&
+      trigger.operation.inputFields &&
+      trigger.operation.inputFields
+        .filter((field: { required: any }) => field && field.required)
+        .map((field: { key: any }) => field.key)) ||
+    [];
+
+  const triggerIsConfigured = Boolean(
+    triggerConfigSubmitted &&
+      requiredTriggerFields.filter(
+        (field: string) =>
+          workflow &&
+          workflow.trigger &&
+          workflow.trigger.input &&
+          workflow.trigger.input[field]
+      ).length === requiredTriggerFields.length
+  );
+
+  const selectedTriggerConnector = connectorsWithTriggers?.find(
+    (connector) =>
+      connector &&
+      connector.name &&
+      connector.name === workflow?.trigger.connector
+  );
+
+  const selectedActionConnector = connectorsWithActions?.find(
+    (connector) =>
+      connector &&
+      connector.name &&
+      connector.name === workflow?.actions[0].connector
+  );
+
+  const availableTriggers =
+    (triggerConnectorIsSet &&
+      selectedTriggerConnector &&
+      selectedTriggerConnector.triggers) ||
+    [];
+
+  const availableActions =
+    (triggerConnectorIsSet &&
+      selectedActionConnector &&
+      selectedActionConnector.actions) ||
+    [];
 
   console.log("workflow", workflow);
 
@@ -67,6 +175,20 @@ export const AppContextProvider = ({ children }: AppContextProps) => {
         setWorkflow,
         connectorsWithActions,
         connectorsWithTriggers,
+        triggerIsSet,
+        actionIsSet,
+        triggerIsAuthenticated,
+        triggerIsConfigured,
+        trigger,
+        availableTriggers,
+        availableActions,
+        action,
+        triggerConnector,
+        actionConnector,
+        triggerConnectorIsSet,
+        actionConnectorIsSet,
+        triggerConfigSubmitted,
+        setTriggerConfigSubmitted,
       }}
     >
       {children}
