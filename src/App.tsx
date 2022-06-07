adaddc92fab4cd64ce9f129ef544b1fffe1f04f5
milukove <milukove@gmail.com>
2022-06-07 06:07:35 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import AppHeader from "./components/AppHeader";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthenticationHandler from "./components/AuthenticationHandler";
 import WorkflowConstructor from "./components/WorkflowConstructor";
 import AppContextProvider from "./context/AppContext";
 
 function App() {
   return (
     <AppContextProvider>
-      <AppHeader />
-      <WorkflowConstructor />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div style={{ padding: "60px 20px 50px" }}>
+                <WorkflowConstructor />
+              </div>
+            }
+          ></Route>
+          <Route path="/auth" element={<AuthenticationHandler />}></Route>
+        </Routes>
+      </BrowserRouter>
     </AppContextProvider>
   );
 }
