0554fcc0bde1e618ac6b91add3d8ed93b6f2614d
milukove <milukove@gmail.com>
2022-06-12 06:16:18 +0300

    rename tabs folder

---
+++ src/components/tabs/Workflows.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WorkflowConstructor from "../workflow/WorkflowConstructor";
+
+type Props = {};
+
+const Workflows = (props: Props) => {
+  return <WorkflowConstructor />;
+};
+
+export default Workflows;
