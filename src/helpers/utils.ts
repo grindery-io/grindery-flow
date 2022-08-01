4cb0e949891173d6da791b0f9240774e0f832ed6
milukove <milukove@gmail.com>
2022-08-02 01:40:06 +0300

    update field validation schema compilation

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -96,34 +96,36 @@ export const getSelfIdCookie = () => {
 
 export const getValidationScheme = (inputFields: Field[]) => {
   const sanitizeType = (type: string) => {
-    if (type === "integer") {
-      return "number";
-    }
-    if (
-      type !== "string" &&
-      type !== "boolean" &&
-      type !== "number" &&
-      type !== "address"
-    ) {
-      return "string";
-    } else {
-      return type;
+    switch (type) {
+      case "integer":
+        return "number";
+      case "text":
+        return "string";
+      case "datetime":
+        return "date";
+      case "file":
+        return "string";
+      case "password":
+        return "string";
+      case "copy":
+        return "string";
+      case "code":
+        return "string";
+      default:
+        return type;
     }
   };
   const schema: any = {};
   inputFields.forEach((field: Field) => {
     schema[field.key] = {
-      type: field.validation?.type || sanitizeType(field.type || ""),
+      type: sanitizeType(field.type || ""),
       optional: !field.required,
       empty: !field.required,
       ...field.validation,
     };
-    if (field.type === "datetime") {
-      schema[field.key].type = "date";
-    }
     if (field.list) {
       schema[field.key].items = {
-        type: field.validation?.type || sanitizeType(field.type || ""),
+        type: sanitizeType(field.type || ""),
         empty: !field.required,
       };
       schema[field.key].type = "array";
