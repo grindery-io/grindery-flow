3de5081ace049b8da5bc77bf310ce7a98f657b77
Egor Miliukov <milukove@gmail.com>
2022-12-08 00:14:53 +0200

    add access checking per app

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -276,7 +276,7 @@ export const AppContextProvider = ({ children }: AppContextProps) => {
 
   const verifyUser = async () => {
     setVerifying(true);
-    const res = await client?.isAllowedUser().catch((err) => {
+    const res = await client?.isAllowedUser("flow").catch((err) => {
       console.error("isAllowedUser error:", err.message);
       setAccessAllowed(false);
     });
--- src/context/SignInContext.tsx
+++ src/context/SignInContext.tsx
@@ -33,7 +33,7 @@ export const SignInContextProvider = ({ children }: SignInContextProps) => {
 
   const verifyUser = async () => {
     setVerifying(true);
-    const res = await client?.isAllowedUser().catch((err) => {
+    const res = await client?.isAllowedUser("gateway").catch((err) => {
       console.error("isAllowedUser error:", err.message);
       setAccessAllowed(false);
     });
