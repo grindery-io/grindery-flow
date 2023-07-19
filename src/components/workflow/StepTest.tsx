88c1de1dc46d7588792d15e0902bc25039404694
Egor Miliukov <milukove@gmail.com>
2023-07-19 21:51:13 +0300

    fix trigger testing

--- src/components/workflow/StepTest.tsx
+++ src/components/workflow/StepTest.tsx
@@ -352,12 +352,7 @@ const StepTest = ({ outputFields }: Props) => {
                 jsonrpc: "2.0",
                 method: "testTrigger",
                 params: {
-                  trigger: {
-                    type: "trigger",
-                    connector: workflow.trigger.connector || "",
-                    operation: workflow.trigger.operation || "",
-                    input: workflow.trigger.input || {},
-                  },
+                  trigger: workflow.trigger,
                   environment: isLocalOrStaging ? "staging" : "production",
                 },
                 id: new Date(),
